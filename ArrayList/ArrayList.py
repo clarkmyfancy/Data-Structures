ed93c934a6bdd54afbc1bdd54a2537cfc276bd3a
clarkmyfancy <clark.jack0@gmail.com>
2020-07-10 11:14:09 -0500

    initial commit

---
+++ ArrayList/ArrayList.py
@@ -0,0 +1,68 @@
+class ArrayList:
+
+    def __init__(self, _head = None, _tail = None):
+        self._head = _head
+        self._tail = _tail
+
+    def count(self):
+        runner = self._head
+        count = 0
+        while(runner != None):
+            count += 1
+            runner = runner._next
+        return count
+
+    def push(self, node):
+        if (self._head == None):
+            self._head = node
+            self._tail = node
+        else:
+            self._tail._next = node
+            self._tail = node
+
+    def pop(self):
+        if (self._head == None):
+            return
+        else:    
+            temp = self._head
+            self._head = self._head._next
+            temp._next = None
+            del temp
+
+    def pretty_print(self):
+        runner = self._head
+        legible_list = ""
+        while (runner != None):
+            if (runner._next == None):
+                legible_list += ("|" + str(runner._value) + "|" + " ~")
+            else:
+                legible_list += ("|" + str(runner._value) + "|" + " -> ")
+            runner = runner._next
+        return legible_list
+
+class Node:
+
+    def __init__(self, _value = None):
+        self._value = _value
+        self._next = None
+
+
+def main():
+    x = ArrayList()
+    n0 = Node(123542634263)
+    n1 = Node(-1)
+    n2 = Node(0)
+    n3 = Node()
+    n4 = Node(312.2134)
+    
+    x.push(n0)
+    x.push(n1)
+    x.push(n2)
+    x.push(n3)
+    x.push(n4)
+
+    print(x.pretty_print())
+
+
+if (__name__ == "__main__"):
+    main()
---
+++ ArrayList/ArrayList_Test.py
@@ -0,0 +1,51 @@
+import unittest
+from ArrayList import ArrayList
+from ArrayList import Node
+
+class TestArrayList(unittest.TestCase):
+
+    def setUp(self):
+	    self.List = ArrayList()
+
+    def tearDown(self):
+	    del self.List
+
+    def test_WhenAddedZeroItems_ListIsEmpty(self):
+        self.assertEqual(0, self.List.count())
+
+    def test_AfterPush1Node_ListHas1Node(self):
+        n = Node()
+        self.List.push(n)
+        self.assertEqual(1, self.List.count())
+    
+    def test_AfterPush2Nodes_ListHas2Nodes(self):
+        n1 = Node()
+        n2 = Node()
+        self.List.push(n1)
+        self.List.push(n2)
+        self.assertEqual(2, self.List.count())
+
+    def test_After1PushAnd1Pop_ListIsEmpty(self):
+        n = Node()
+        self.List.push(n)
+        self.List.pop()
+        self.assertEqual(0, self.List.count())
+
+    def test_AfterAttemptToPopFromEmtpyArray_ListRemainsEmpty(self):
+        self.List.pop()
+        self.assertEqual(0, self.List.count())
+
+    def test_AfterPush1Node_PrintsPrettily(self):
+        n = Node(15)
+        self.List.push(n)
+        self.assertEqual("|15| ~", self.List.pretty_print())
+
+    def test_AfterPush2Nodes_PrintsPrettily(self):
+        n0 = Node(12342342)
+        n1 = Node(-123)
+        self.List.push(n0)
+        self.List.push(n1)
+        self.assertEqual("|12342342| -> |-123| ~", self.List.pretty_print())
+
+if (__name__ == '__main__'):
+	unittest.main()
