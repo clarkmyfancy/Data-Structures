b754f1beb23d6749fe639345d8076ec2093ce04d
Jack Conrad Clark <clark.jack0@gmail.com>
2021-05-07 08:08:09 -0500

    test that travis is working

--- SinglyLinkedList/SinglyLinkedList_pytest_test.py
+++ SinglyLinkedList/SinglyLinkedList_pytest_test.py
@@ -8,3 +8,7 @@ class TestClass:
     def test_he_has_an_h_in_it(self):
         x = "he"
         assert "h" in x 
+    
+    def test_travis_ci_hooks_are_working(self):
+        x = "she"
+        assert "s" in x
