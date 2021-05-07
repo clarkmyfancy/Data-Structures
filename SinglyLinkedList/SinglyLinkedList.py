966fc56b64bfe38c52dddd6f03c1b7010d65769d
Jack Conrad Clark <clark.jack0@gmail.com>
2021-05-06 22:07:23 -0500

    extract out main to its own file

--- SinglyLinkedList/SinglyLinkedList.py
+++ SinglyLinkedList/SinglyLinkedList.py
@@ -39,30 +39,3 @@ class SinglyLinkedList:
                 legible_list += ("|" + str(runner._value) + "|" + " -> ")
             runner = runner._next
         return legible_list
-
-class Node:
-
-    def __init__(self, _value = None):
-        self._value = _value
-        self._next = None
-
-
-def main():
-    x = SinglyLinkedList()
-    n0 = Node(123542634263)
-    n1 = Node(-1)
-    n2 = Node(0)
-    n3 = Node()
-    n4 = Node(312.2134)
-    
-    x.push(n0)
-    x.push(n1)
-    x.push(n2)
-    x.push(n3)
-    x.push(n4)
-
-    print(x.pretty_print())
-
-
-if (__name__ == "__main__"):
-    main()
